622d0aac13f33d6a913ce112f337f52f0daf683e
Trayan Iliev <office@iproduct.org>
2019-12-06 14:21:44 +0200

    pulsar basic client demo initial commit

---
+++ pulsar-basic-demo/src/main/java/pulsar/PulsarBasicClient.java
@@ -0,0 +1,49 @@
+package pulsar;
+
+import org.apache.pulsar.client.api.*;
+
+public class PulsarBasicClient {
+
+    public static void main(String[] args) throws PulsarClientException, InterruptedException {
+        PulsarClient client = PulsarClient.builder()
+                .serviceUrl("pulsar://localhost:6650")
+                .build();
+        Producer<String> producer = client.newProducer(Schema.STRING)
+                .topic("my-topic")
+                .create();
+
+        Consumer<String> consumer = client.newConsumer(Schema.STRING)
+                .topic("my-topic")
+                .subscriptionName("my-subscription")
+                .subscriptionType(SubscriptionType.Exclusive)
+                .subscribe();
+
+        // You can then send messages to the broker and topic you specified:
+        producer.send("My message 1");
+        producer.sendAsync("my-async-message 1").thenAccept(msgId -> {
+            System.out.printf("Message with ID %s successfully sent", msgId);
+        });
+        producer.send("My message 2");
+        producer.send("My message 3");
+        producer.send("My message 4");
+
+        for(int i = 0; i < 4; i++) {
+            consumer.receiveAsync().thenAccept(msg -> {
+                System.out.printf("!!!! Message received: %s\n\n", msg.getValue());
+                try {
+                    consumer.acknowledge(msg);
+                } catch (PulsarClientException e) {
+                    e.printStackTrace();
+                }
+            });//.join();
+        }
+
+        Thread.sleep(10000);
+        producer.close();
+        consumer.close();
+        client.close();
+        System.out.println("Demo finished.");
+
+
+    }
+}
